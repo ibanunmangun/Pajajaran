c5735a20dc2bea56bf8725a3435648733ba12009
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-01-07 08:10:44 +0700

    Add files via upload

---
+++ data/Point_of_Interest_3.js
@@ -0,0 +1,25 @@
+var json_Point_of_Interest_3 = {
+"type": "FeatureCollection",
+"name": "Point_of_Interest_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Monumen KTT Non Blok", "X": 107.5926119, "Y": -6.9072664, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.592611900000065, -6.907266399999969 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Pandu.pajajaran", "X": 107.593772, "Y": -6.9056502, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.593772, -6.905650199999968 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemakaman Pandu Memorial", "X": 107.5923256, "Y": -6.9005117, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.592325600000038, -6.900511699999981 ] } },
+{ "type": "Feature", "properties": { "Name": "Cakwe Lie Tjay Tat", "X": 107.601178, "Y": -6.9080506, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.601178, -6.908050599999967 ] } },
+{ "type": "Feature", "properties": { "Name": "Wyataguna", "X": 107.6018035, "Y": -6.9064673, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.601803500000074, -6.906467299999974 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Makan Ma' Uneh", "X": 107.6028964, "Y": -6.9067999, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.602896400000077, -6.906799899999953 ] } },
+{ "type": "Feature", "properties": { "Name": "Pabrik Kina Kimia Farma Bandung", "X": 107.6031829, "Y": -6.9082876, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.603182900000036, -6.908287599999937 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Miftahul Barokah", "X": 107.593753, "Y": -6.907464, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.593753, -6.907464 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Pertamina 34.40115", "X": 107.585948, "Y": -6.907057, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.585948, -6.907057 ] } },
+{ "type": "Feature", "properties": { "Name": "Apotek Rama", "X": 107.590052, "Y": -6.90743, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.590052, -6.90743 ] } },
+{ "type": "Feature", "properties": { "Name": "Western Union Pajajaran", "X": 107.595715, "Y": -6.906466, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.595715, -6.906466 ] } },
+{ "type": "Feature", "properties": { "Name": "Foodcourt GOR Pajajaran", "X": 107.60119, "Y": -6.907758, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.60119, -6.907758 ] } },
+{ "type": "Feature", "properties": { "Name": "Toilet GOR Pajajaran", "X": 107.601673, "Y": -6.907738, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.601673, -6.907738 ] } },
+{ "type": "Feature", "properties": { "Name": "GOR Pajajaran", "X": 107.601673, "Y": -6.907738, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.601673, -6.907738 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Baitush Sholihin", "X": 107.600675, "Y": -6.904365, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.600675, -6.904365 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Bethany Indonesia", "X": 107.597312, "Y": -6.907271, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.597312, -6.907271 ] } },
+{ "type": "Feature", "properties": { "Name": "RSIA Melinda", "X": 107.603294, "Y": -6.906971, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.603294, -6.906971 ] } },
+{ "type": "Feature", "properties": { "Name": "Kimia Farma Pajajaran", "X": 107.602803, "Y": -6.907074, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.602803, -6.907074 ] } }
+]
+}
